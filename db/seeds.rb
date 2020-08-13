b6633922f7d0d8b37adcc97c41007ba3030769e3
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-13 19:25:42 +0200

    back of cocktail app done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |i|
+  name = i["strIngredient1"]
+  record = Ingredient.new(name: name)
+  record.save!
+end
