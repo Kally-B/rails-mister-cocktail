e0f68353034b29b6be9c3025dd0e940540e43fa2
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-14 10:52:32 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,38 @@
 require 'open-uri'
 require 'json'
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-ingredients = JSON.parse(open(url).read)
+# ingredients = JSON.parse(open(url).read)
 
-ingredients["drinks"].each do |i|
-  name = i["strIngredient1"]
-  record = Ingredient.new(name: name)
-  record.save!
+# ingredients["drinks"].each do |i|
+#   name = i["strIngredient1"]
+#   record = Ingredient.new(name: name)
+#   record.save!
+# end
+
+# cocktail_url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
+
+numbers = (1..15).to_a
+
+20.times do
+  random_url = open('https://www.thecocktaildb.com/api/json/v1/1/random.php').read
+  cocktail_details = JSON.parse(random_url)
+  details = cocktail_details['drinks'][0]
+  cocktail = Cocktail.create(name: details['strDrink'])
+  numbers.each do |x|
+    ingredient = details["strIngredient#{x}"]
+    measure = details["strMeasure#{x}"]
+    if (ingredient.nil? == false) && (measure.nil? == false) && Ingredient.find_by(name: ingredient).nil?
+      new_ingredient = Ingredient.create(name: ingredient)
+      dose = Dose.new(description: measure, ingredient: new_ingredient)
+      dose.cocktail = cocktail
+      dose.save!
+    elsif (ingredient.nil? == false) && (measure.nil? == false) && (Ingredient.find_by(name: ingredient).nil? == false)
+      existing_ingredient = Ingredient.find_by(name: ingredient)
+      dose = Dose.new(description: measure, ingredient: existing_ingredient)
+      dose.cocktail = cocktail
+      dose.save!
+    end
+  end
 end
